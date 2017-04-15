99bf40154bfa36c1b8d98c5182060390e5742e84
Tomas <tomasmaggio@gmail.com>
2017-04-15 20:08:11 +1200

    Update docker-entrypoint.sh
    Customization stuff

--- scripts/docker-entrypoint.sh
+++ scripts/docker-entrypoint.sh
@@ -8,6 +8,11 @@ mysql -u root < /usr/share/zoneminder/db/zm_create.sql
 mysql -u root -e "GRANT ALL ON zm.* to 'zmuser'@localhost identified by 'zmpass';"
 mysqladmin reload
 
+# Customize ZM
+line_old='<h2 id="title"><a href="http://www.zoneminder.com" target="ZoneMinder">ZoneMinder</a>'
+line_new='<h2 id="title"><a href="https://www.doublec.tv" target="DoubleC">DoubleC.tv</a>'
+sed -i "s%$line_old%$line_new%g" /usr/share/zoneminder/www/skins/classic/views/console.php
+
 systemctl enable zoneminder
 /etc/init.d/zoneminder start
 /etc/init.d/apache2 restart
