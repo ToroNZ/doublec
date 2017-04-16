b6e761daedefae67dfa3af851d6920b282c0af79
Tomas <tomasmaggio@gmail.com>
2017-04-16 17:51:05 +1200

    Change PATH_ZMS

--- scripts/docker-entrypoint.sh
+++ scripts/docker-entrypoint.sh
@@ -13,8 +13,10 @@ mysqladmin reload
 line_old='<h2 id="title"><a href="http://www.zoneminder.com" target="ZoneMinder">ZoneMinder</a>'
 line_new='<h2 id="title"><a href="https://www.doublec.tv" target="DoubleC">DoubleC.tv</a>'
 sed -i "s%$line_old%$line_new%g" /usr/share/zoneminder/www/skins/classic/views/console.php
+
 # Favicon
 wget -O /usr/share/zoneminder/www/graphics/favicon.ico "https://filer.sensaway.co.nz/f/a8a654f726/?dl=1"
+
 # Change browser tab title
 line_old1='<title><?php echo ZM_WEB_TITLE_PREFIX ?></title>'
 line_new1='<title>DoubleC.tv - Console</title>'
@@ -22,6 +24,14 @@ sed -i "s%$line_old1%$line_new1%g" /usr/share/zoneminder/www/skins/classic/views
 line_old2='<title><?php echo ZM_WEB_TITLE_PREFIX ?> - <?php echo validHtmlStr($title) ?></title>'
 line_new2='<title>DoubleC.tv - Console</title>'
 sed -i "s%$line_old2%$line_new2%g" /usr/share/zoneminder/www/skins/classic/includes/functions.php
+
+### Change PATH_ZMS under options:
+##From "/cgi-bin/nph-zms" to "/zoneminder/cgi-bin/nph-zms"
+mkdir /usr/lib/cgi-bin/nph-zms
+ln -s /usr/lib/zoneminder/cgi-bin/nph-zms /usr/lib/cgi-bin/nph-zms
+line_old3='ScriptAlias /zm/cgi-bin "/usr/lib/zoneminder/cgi-bin"'
+line_new3='ScriptAlias /zoneminder/cgi-bin "/usr/lib/zoneminder/cgi-bin"'
+sed -i "s%$line_old3%$line_new3%g" /etc/apache2/conf-available/zoneminder.conf
 
 systemctl enable zoneminder
 /etc/init.d/zoneminder start
